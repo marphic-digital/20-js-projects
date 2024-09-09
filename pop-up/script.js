e7736938dc50e0e28e80f99d3e24e356cee49101
marphic-digital <marphicdigital@gmail.com>
2024-09-09 21:08:26 +0500

    'testimonial-slides'

--- pop-up/script.js
+++ pop-up/script.js
@@ -2,9 +2,7 @@ const openModalBtnElement = document.querySelector("#openModal");
 const modalEl = document.querySelector(".modal");
 const modalContentEl = document.querySelector(".modal__content");
 
-console.log(modalContentEl);
-
-openModalBtnElement.addEventListener("click", () => {
+const z = openModalBtnElement.addEventListener("click", () => {
   modalEl.classList.add("open");
 });
 
